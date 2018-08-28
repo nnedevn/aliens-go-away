37fb03c47c6fcbf17dcdea5f9d7994413992cc2b
Nikolay Nedev <nedevn@gmail.com>
2018-08-28 15:34:00 -0700

    Adds calculateAngle

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,17 @@
-  const initialState = {
-    message: `Integrating React with Redux`,
-  };
+import { MOVE_OBJECTS } from '../actions';
+import moveObjects from './moveObjects';
 
-  function reducer(state = initialState){
-    return state;
+const initialState = {
+  angle: 45,
+};
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case MOVE_OBJECTS:
+      return moveObjects(state, action);
+    default:
+      return state;
   }
+}
 
-  export default reducer;
+export default reducer;
--- src/utils/formulas.js
+++ src/utils/formulas.js
@@ -15,3 +15,18 @@ export const pathFromBezierCurve = (cubicBezierCurve) => {
     ${endingAxis.x} ${endingAxis.y}
   `;
 }
+
+export const radiansToDegrees = radians => ((radians * 180) / Math.PI);
+
+export const calculateAngle = (x1, y1, x2, y2) => {
+  if (x2 >= 0 && y2 >= 0) {
+    return 90;
+  } else if (x2 < 0 && y2 >= 0) {
+    return -90;
+  }
+
+  const dividend = x2 - x1;
+  const divisor = y2 - y1;
+  const quotient = dividend / divisor;
+  return radiansToDegrees(Math.atan(quotient)) * -1;
+};
