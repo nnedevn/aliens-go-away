00a0d863035a7f25b2ac109e5e725e2fa69a8daf
Nikolay Nedev <nedevn@gmail.com>
2018-08-27 22:23:41 -0700

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>here be fun things</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
