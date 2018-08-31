294c8f700beee46c5f3194f50814ad8f54e7693c
Nikolay Nedev <nedevn@gmail.com>
2018-08-31 12:36:00 -0700

    Adds Heart,jsx

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -7,6 +7,7 @@ import CannonBase from "./CannonBase";
 import CannonBall from "./CannonBall";
 import CurrentScore from "./CurrentScore";
 import FlyingObject from './FlyingObject';
+import Heart from './Heart';
 
 const Canvas = props => {
   const viewBox = [
@@ -36,6 +37,8 @@ const Canvas = props => {
       <CurrentScore score={15} />
       <FlyingObject position={{x: -150, y: -300}}/>
       <FlyingObject position={{x: 150, y: -300}}/>
+      <Heart position={{x: -300, y:45}}/>
+
     </svg>
 
   );
